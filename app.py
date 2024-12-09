741244913fcc3ac547b443392f7e660daa4e63db
hhhwhn123 <WANG2201@e.ntu.edu.sg>
2024-12-09 08:22:11 +0000

    a

--- app.py
+++ app.py
@@ -4,6 +4,8 @@
 from flask import Flask
 from flask import render_template, request
 import textblob
+import google.generativeai as genai
+import os
 
 app = Flask(__name__)
 
@@ -26,5 +28,27 @@ def SA_result():
     r = textblob.TextBlob(q).sentiment
     return(render_template("SA_result.html",r=r))
 
+@app.route("/AI", methods=["GET", "POST"])
+def AI():
+    
+    return render_template("AI.html")
+
+@app.route("/AI_result", methods=["GET","POST"])
+def AI_result():
+    q = request.form.get("q")
+    r = ask_gemini(q)
+    return (render_template("AI_result.html", r=r))
+
+def ask_gemini(q):
+    api = os.getenv("maskersuite")
+    genai.configure(api_key=api)
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    r = model.generate_content(q)
+    text = r.candidates[0].content.parts[0].text
+    return text
+
 if __name__ == "__main__":
     app.run()
+    api = os.getenv("maskersuite")
+    genai.configure(api_key=api)
+    m = genai.GenerativeModel("gemini-1.5-flash")
